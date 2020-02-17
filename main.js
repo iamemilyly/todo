09ce8130708af9c7a4167d3b0d293e7f36357814
Emily Ding <eding@andrew.cmu.edu>
2020-02-17 15:33:28 -0500

    all js changes

--- main.js
+++ main.js
@@ -4,4 +4,21 @@ function addNewList() {
 
 function addListItem() {
     console.log('hello world');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item");
+    var newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item){
+	item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) {
+	if (item.checked) {
+		item.parentNode.style.textDecoration = "line-through";
+	} else {
+		item.parentNode.style.textDecoration = "none";
+	}
 }
